dcbd96df158f34560e1daab89c20b99ee7861ae2
Breno Andrade <brebritto@gmail.com>
2024-05-21 18:07:07 -0300

    1.01

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
 ﻿using Estudo.models;
 
 // Tipos de dados principais.
-string apresentacao = "Olá, isso é uma string";
+string apresentacao = "Ola, isso e uma string";
 int quantidade = 10;
 double altura = 1.80;
 decimal preco = 19.90M;
 bool condicao = true;
 
 Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
-Console.WriteLine("Valor da variável altura: " + altura);
-Console.WriteLine("Valor da variável preco: " + preco);
-Console.WriteLine("Valor da variável condicao: " + condicao);
+Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variavel preco: " + preco);
+Console.WriteLine("Valor da variavel condicao: " + condicao);
 
 // Forma I de instanciar a classe.
 Pessoa p1 = new Pessoa();
@@ -34,3 +34,81 @@ Pessoa p3 = new()
     Idade = 21
 };
 p3.Apresentar();
+
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Tipos de dados e Conversoes");
+
+int a = Convert.ToInt32("5");
+Console.WriteLine (a);
+// Pode ser nulo
+int b = int.Parse("5");
+Console.WriteLine (b);
+// Não pode ser nulo
+string c = a.ToString();
+// Converter para string
+Console.WriteLine(c + b);
+// Tentar converter
+Console.WriteLine(int.TryParse("15", out a));
+
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Condicionais if - else if - else");
+
+Console.WriteLine("Digite a quantidade em estoque: ");
+int quantidadeEstoque = int.Parse(Console.ReadLine());
+// ReadLine le uma entrada em forma de string
+Console.WriteLine("Digite a quantidade da compra: ");
+int quantidadeCompra = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+
+bool vendaPossivel = quantidadeEstoque > quantidadeCompra;
+if (vendaPossivel)
+{
+    Console.WriteLine("Venda e possivel");
+} 
+else if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Venda invalida");
+}
+else
+{
+    Console.WriteLine("Venda nao e possivel");
+}
+
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Condicionais switch");
+
+Console.WriteLine("Digite uma letra: ");
+string letra = Console.ReadLine();
+
+switch(letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+    default:
+        Console.WriteLine("Consoante");
+        break;
+}
+
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Condicionais OR(||) - AND(&&) - NOT(!)");
+
+int valor = 9;
+
+if (valor > 3 && valor < 8)
+{
+    Console.WriteLine("Valor dentre 4 a 7");
+} else if (valor < 4 || valor > 7)
+{
+    Console.WriteLine("Valor abaixo de 4 ou acima de 7");
+}
+if (!(valor % 2 == 0))
+{
+    Console.WriteLine("Valor nao e par");
+}
