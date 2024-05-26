a03ed86732490a5e3bed64f8464e6dec45370dd1
Breno Andrade <brebritto@gmail.com>
2024-05-25 23:26:39 -0300

    1.06

--- Program.cs
+++ Program.cs
@@ -3,20 +3,21 @@ using Estudo.models;
 using EstudoCSharp.models;
 
 // Tipos de dados principais.
-string apresentacao = "Ola, isso e uma string";
+string apresentacao = "Olá, isso é uma string";
 int quantidade = 10;
 double altura = 1.80;
+// Colocar um M no final do número decimal
 decimal preco = 19.90M;
 bool condicao = true;
 
 Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variavel quantidade: " + quantidade);
-Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variavel preco: " + preco);
-Console.WriteLine("Valor da variavel condicao: " + condicao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
 
 Console.WriteLine("-----------------------------");
-Console.WriteLine("Tipos de dados e Conversoes");
+Console.WriteLine("Tipos de dados e conversão");
 
 int a = Convert.ToInt32("5");
 Console.WriteLine (a);
@@ -35,7 +36,7 @@ Console.WriteLine("Condicionais if - else if - else");
 
 // Console.WriteLine("Digite a quantidade em estoque: ");
 // int quantidadeEstoque = int.Parse(Console.ReadLine());
-// ReadLine le uma entrada em forma de string
+// ReadLine lê uma entrada em forma de string
 int quantidadeEstoque = 10;
 // Console.WriteLine("Digite a quantidade da compra: ");
 // int quantidadeCompra = int.Parse(Console.ReadLine());
@@ -45,17 +46,20 @@ Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
 Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
 
 bool vendaPossivel = quantidadeEstoque > quantidadeCompra;
+// Se for true
 if (vendaPossivel)
 {
-    Console.WriteLine("Venda e possivel");
-} 
+    Console.WriteLine("Venda é possível");
+}
+// Outro if para ser testado
 else if (quantidadeCompra == 0)
 {
-    Console.WriteLine("Venda invalida");
+    Console.WriteLine("Venda inválida");
 }
+// Se for false
 else
 {
-    Console.WriteLine("Venda nao e possivel");
+    Console.WriteLine("Venda não é possível");
 }
 
 Console.WriteLine("-----------------------------");
@@ -67,12 +71,14 @@ string letra = "a";
 
 switch(letra)
 {
+    // Passa por cada caso
     case "a":
     case "e":
     case "i":
     case "o":
     case "u":
         Console.WriteLine("Vogal");
+        // Sai do caso sem ler os próximos
         break;
     default:
         Console.WriteLine("Consoante");
@@ -83,14 +89,17 @@ Console.WriteLine("-----------------------------");
 Console.WriteLine("Condicionais OR(||) - AND(&&) - NOT(!)");
 
 int valor = 9;
-
+// Precisa que os dois sejam verdadeiros
 if (valor > 3 && valor < 8)
 {
     Console.WriteLine("Valor dentre 4 a 7");
-} else if (valor < 4 || valor > 7)
+}
+// Precisa que uma das condições seja verdadeira
+else if (valor < 4 || valor > 7)
 {
     Console.WriteLine("Valor abaixo de 4 ou acima de 7");
 }
+// Precisa que a condição seja oposta
 if (!(valor % 2 == 0))
 {
     Console.WriteLine("Valor nao e par");
@@ -99,6 +108,7 @@ if (!(valor % 2 == 0))
 Console.WriteLine("-----------------------------");
 Console.WriteLine("Operadores");
 
+// Usando a classe Calculadora
 Calculadora calc = new Calculadora();
 calc.Somar(4, 5);
 calc.Subtrair(5, 4);
@@ -114,14 +124,17 @@ Console.WriteLine("-----------------------------");
 Console.WriteLine("FOR");
 
 int numero = 5;
+// Loop com um contador fixo
 for (int contador = 1; contador <= 10; contador++)
 {
     Console.WriteLine($"{numero} * {contador} = {numero * contador}");
 }
 
 Console.WriteLine("WHILE");
+
 int numero1 = 5;
 int contador1 = 1;
+// Loop sem contador e que precisa de uma condição para encerrar
 while (contador1 <= 10)
 {
     Console.WriteLine($"{numero1} * {contador1} = {numero1 * contador1}");
@@ -129,8 +142,10 @@ while (contador1 <= 10)
 }
 
 Console.WriteLine("DO WHILE");
+
 int numero2 = 5;
 int contador2 = 1;
+// Loop que inicia um laço antes de ser testado e continua até uma condição ser atentida
 do 
 {
     Console.WriteLine($"{numero2} * {contador2} = {numero2 * contador2}");
@@ -138,9 +153,12 @@ do
 } while (contador2 <= 10);
 
 Console.WriteLine("-----------------------------");
+// Agrupamento de dados de mesmo tipo
 Console.WriteLine("Array");
 
+// Criar um array do tipo inteiro de tamanho 3
 int[] arrayInteiros = new int[3];
+// Adicionar valores ao array
 arrayInteiros[0] = 13;
 arrayInteiros[1] = 42;
 arrayInteiros[2] = 27;
@@ -153,6 +171,7 @@ for (int i = 0; i < arrayInteiros.Length; i++)
 Console.WriteLine("-----------------------------");
 Console.WriteLine("Array - Resize");
 
+// Função para alterar o tamanho do array
 Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
 foreach (int valor2 in arrayInteiros)
 {
@@ -163,6 +182,7 @@ Console.WriteLine("-----------------------------");
 Console.WriteLine("Array - Copy");
 
 int[] arrayInteiros2 = new int[arrayInteiros.Length];
+// Função para copiar um array em outro com uma quantidade de dados copiados definida
 Array.Copy(arrayInteiros, arrayInteiros2, arrayInteiros.Length);
 foreach (int valor2 in arrayInteiros2)
 {
@@ -174,25 +194,34 @@ Console.WriteLine("-----------------------------");
 Console.WriteLine("Lists");
 
 List<string> listaString = new List<string>();
+// Adicionar elementos
 listaString.Add("BA");
 listaString.Add("SP");
 listaString.Add("RJ");
-Console.WriteLine("ADD");
+Console.WriteLine("Adicionando elementos a lista");
+
 for (int i = 0; i < listaString.Count; i++)
 {
     Console.WriteLine($"Posicao N{i} - {listaString[i]}");
 }
+
+// Remover elementos
 listaString.Remove("SP");
-Console.WriteLine("REMOVE");
+Console.WriteLine("SP removido");
+
 for (int i = 0; i < listaString.Count; i++)
 {
     Console.WriteLine($"Posicao N{i} - {listaString[i]}");
 }
+
 Console.WriteLine("-----------------------------");
 Console.WriteLine("Propriedades, metodos e construtores");
+
+// Instanciando objeto a partir da classe Pessoa
 Pessoa p1 = new Pessoa();
 p1.Nome = "Pedro";
 p1.Sobrenome = "Almeida";
+// Forma simplificada de instanciar
 Pessoa p2 = new("Mateus", "Andrade");
 
 Curso cursoDeIngles = new()
@@ -210,30 +239,35 @@ cursoDeIngles.ListarAlunos();
 Console.WriteLine("-----------------------------");
 Console.WriteLine("Ajustando valores");
 
+// Definir nova cultura como default
 // CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
         
 decimal dinheiro = 1582.40M;
 
+// Alterar cultura localmente
 Console.WriteLine(dinheiro.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
 Console.WriteLine(dinheiro.ToString("C1", CultureInfo.CreateSpecificCulture("en-US")));
 
 double porcentagem = .3412;
-
+// Formatar como porcentagem
 Console.WriteLine(porcentagem.ToString("P"));
 
 double numero3 = 173412;
-
+// Formatar em três duplas de números
 Console.WriteLine(numero3.ToString("##-##-##"));
 
+// Pega a data e hora local
 // DateTime data = DateTime.Now;
 DateTime data = DateTime.Parse("20/07/2024 18:00");
 
+// Formatar data e hora
 Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
 Console.WriteLine(data.ToShortDateString());
 Console.WriteLine(data.ToShortTimeString());
 
 Console.WriteLine("-----------------------------");
-Console.WriteLine("Excecoes");
+// Tenta realizar um ação e trata possíveis exceções
+Console.WriteLine("Exceções");
 
 // Ler Arquivos
 // string[] linhas = File.ReadAllLines("arquivos/leitura.txt");
@@ -241,6 +275,8 @@ Console.WriteLine("Excecoes");
 // {
 //     Console.WriteLine(linha);
 // }
+
+// Tenta realizar algo e caso encontre uma exceção executa o catch
 try
 {
     string[] linhas = File.ReadAllLines("arquivos/leitura2.txt");
@@ -249,6 +285,7 @@ try
         Console.WriteLine(linha);
     }
 }
+// Executa caso encontra a exceção definida
 catch (FileNotFoundException ex)
 {
     Console.WriteLine($"Arquivo nao encontrado. {ex.Message}");
@@ -261,18 +298,21 @@ catch (Exception ex)
 {
     Console.WriteLine($"Ocorreu uma excecao generica. {ex.Message}");
 }
+// Sempre executa o finally independente de exceções
 finally 
 {
     Console.WriteLine("Chegou ate aqui.");
-    // throw new Exception("Ocorreu uma excecao.");
 }
+
+// Cria e dispara exceções
+// throw new Exception("Ocorreu uma excecao.");
 
 Console.WriteLine("-----------------------------");
 
 //Coleção que segue o padrão FIFO - Primeiro a entrar, primeiro a sair
 Console.WriteLine("Coleções - Fila");
 
-// Criar um fila
+// Criar uma fila
 Queue<int> fila = new();
 
 // Adicionar elementos a fila
